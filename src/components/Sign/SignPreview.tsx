24b61170e250a7db83b9040cd74164c88c0332fa
Jack Hermanson <jack.hermanson@live.com>
2021-06-03 13:25:28 -0600

    start showing preview

--- src/components/Sign/SignPreview.tsx
+++ src/components/Sign/SignPreview.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "../../css/sign-preview.css";
+import {useStoreState} from "../../store";
 
 export const SignPreview: React.FC = () => {
+
+    const title = useStoreState(state => state.title);
+    const titleFontSize = useStoreState(state => state.titleFontSize);
+    const logoPlacement = useStoreState(state => state.logoPlacement);
+
     return (
         <div id="sign-preview">
-            <p>Sign preview</p>
+            {renderTitle()}
         </div>
     );
+
+    function renderTitle() {
+        return (
+            <h1 style={{
+                textAlign: logoPlacement,
+                fontSize: titleFontSize
+            }}>{title}</h1>
+        );
+    }
 }
