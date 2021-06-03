030f8abd220819f3e16f35499277767cd40b3c5b
Jack Hermanson <jack.hermanson@live.com>
2021-06-03 09:51:27 -0600

    set up grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./css/main.css";
+import {FormContainer} from "./components/Form/FormContainer";
 
 export const App: React.FC = () => {
     return (
-        <React.Fragment>
-            <p>This is the app.</p>
-        </React.Fragment>
-    )
+        <div className="main-container">
+            <FormContainer />
+            <div style={{backgroundColor: "red"}}>
+                One
+            </div>
+        </div>
+    );
 }
---
+++ src/components/Form/FormContainer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../css/form.css";
+
+export const FormContainer: React.FC = () => {
+    return (
+        <div>
+            <p>Form container</p>
+        </div>
+    );
+}
