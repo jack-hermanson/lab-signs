a42366547aec65f1d2db3a6f0aa4f9a456feb132
Jack Hermanson <jack.hermanson@live.com>
2021-06-03 12:14:35 -0600

    orientation state

--- src/components/Form/Orientation.tsx
+++ src/components/Form/Orientation.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import {Col, FormGroup, Row, Label, Input} from "reactstrap";
 import {FormSectionTitle} from "./FormSectionTitle";
+import {useStoreActions, useStoreState} from "../../store";
+import {LogoPlacement} from "../../models/LogoPlacement";
+import {ImagePlacement} from "../../models/ImagePlacement";
 
 export const Orientation: React.FC = () => {
+
+    const logoPlacement = useStoreState(state => state.logoPlacement);
+    const setLogoPlacement = useStoreActions(actions => actions.setLogoPlacement);
+    const imagePlacement = useStoreState(state => state.imagePlacement);
+    const setImagePlacement = useStoreActions(state => state.setImagePlacement);
+
     return (
         <FormGroup>
             <FormSectionTitle title="Orientation" />
@@ -18,9 +27,14 @@ export const Orientation: React.FC = () => {
         return (
             <Col>
                 <Label for={id} className="form-label">CHHS Logo Placement</Label>
-                <Input id={id} type="select">
-                    <option>Left</option>
-                    <option>Center</option>
+                <Input
+                    value={logoPlacement}
+                    id={id}
+                    type="select"
+                    onChange={e => setLogoPlacement(e.target.value as LogoPlacement)}
+                >
+                    <option value="center">Center</option>
+                    <option value="left">Left</option>
                 </Input>
             </Col>
         );
@@ -31,9 +45,14 @@ export const Orientation: React.FC = () => {
         return (
             <Col>
                 <Label for={id} className="form-label">Image Placement</Label>
-                <Input id={id} type="select">
-                    <option>Top</option>
-                    <option>Bottom</option>
+                <Input
+                    id={id}
+                    type="select"
+                    value={imagePlacement}
+                    onChange={e => setImagePlacement(e.target.value as ImagePlacement)}
+                >
+                    <option value="top">Top</option>
+                    <option value="bottom">Bottom</option>
                 </Input>
             </Col>
         );
--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,25 @@
 import {createStore, createTypedHooks, action, Action} from "easy-peasy";
+import {LogoPlacement} from "./models/LogoPlacement";
+import {ImagePlacement} from "./models/ImagePlacement";
 
 interface StoreModel {
-    logoPlacement: "left" | "center";
+    logoPlacement: LogoPlacement;
+    setLogoPlacement: Action<StoreModel, LogoPlacement>;
+
+    imagePlacement: ImagePlacement;
+    setImagePlacement: Action<StoreModel, ImagePlacement>;
 }
 
 export const store = createStore<StoreModel>({
     logoPlacement: "center",
+    setLogoPlacement: action((state, payload) => {
+        state.logoPlacement = payload;
+    }),
+
+    imagePlacement: "top",
+    setImagePlacement: action((state, payload) => {
+        state.imagePlacement = payload;
+    }),
 });
 
 const typedHooks = createTypedHooks<StoreModel>();
